5d51ed5d180df5568ef5a4684aeb4fdadd6f4c39
joabeplay1 <joabejonatas186@gmail.com>
2026-07-08 22:44:04 -0300

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx' // Certifique-se de que o nome é exatamente este
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Olá, meu app está funcionando!</h1>
+    <App />
   </React.StrictMode>
 )
